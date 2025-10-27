916c0d2b8829c33e6696524c141147380e689c10
ucd-smr-k3 <ucd-smr-k3@fujinet.net>
2025-10-27 17:46:28 +0700

    remove src

--- src/components/common/buttons/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CircleButton } from './CircleButton';
-export { default as DropCircle } from './DropCircle';
-export { default as DotButton } from './DotButton';
--- src/components/common/icons/index.js
+++
@@ -1,2 +0,0 @@
-export { default as PlayIcon } from './PlayIcon';
-export { default as PauseIcon } from './PauseIcon';
